3da76ccc461c4f63ef0a4bd967c27470e4834d85
SunnyDVwTt8Pb <123383802+SunnyDVwTt8Pb@users.noreply.github.com>
2023-10-19 22:32:43 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header.css';
+import headerImage from './me.jpg'; 
+
+function Header() {
+  return (
+    <header className="header">
+      
+    
+    <div className="header-content">
+        <img src={headerImage} alt="Header Image" className="header-image" />
+        <h1 className="header-name">Chennupati Venkata Lakshman </h1>
+      </div>
+      <h3 >I am third year senior at vellore institute of technology(VIT) in Amaravathi <br/>
+        ,Currently pursuing 
+        CSE specialization in Artificial Inteligence and Machine Learing.
+        Enjoys reading Books,browsing internet,playing Games.<br/>
+        I am a self motivating ,Honest and Straight forward person.
+
+      </h3>
+    </header>
+  );
+}
+
+export default Header;
+
+
